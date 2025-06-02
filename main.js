af2a7872caac54f262004532cf128392320e550e
Caetano Bordin <hyperplayer2012@gmail.com>
2025-06-02 15:00:55 -0300

    Save 4 - Diário Emocional

---
+++ main.js
@@ -0,0 +1,4 @@
+function analisarHumor(texto) {
+    const positivo = ['feliz', 'alegre', 'animado', 'grato', 'ótimo', 'bem', 'contente', 'calmo', 'otimista' ]
+    const negativo = ['triste', 'cansado', 'chateado', 'péssimo', 'mal', 'deprimido', 'ansioso', 'estressado', 'raiva', 'pessimista', 'medroso']
+}
